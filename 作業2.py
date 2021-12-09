dc8757d1d3260cf1b5b83804ac34d09137b188d2
NTUT110441524 <95857769+NTUT110441524@users.noreply.github.com>
2021-12-09 20:54:20 +0800

    Update 作業2.py

--- 作業2.py
+++ 作業2.py
@@ -1,5 +1,5 @@
-練習二
-程式
+#練習二
+#程式
 #變化考題：變化考題：(1)請加入轉速資料的收集，假若轉速 >= 標準轉速的時候，要輸出警訊，提醒駕駛注意。
 #變化考題：變化考題：(2)油箱只剩0.5格的時候，怎麼辦??
 car_simulation = {'gas_warning':1, 'speed_limit':100, 'temp_warning':30, '轉速':12000}
@@ -8,21 +8,22 @@ speed= st.number_input(input('車速的資料收集:限速100 =>'))
 temp = st.number_input(input('溫度的資料收集:限溫30 =>'))
 轉速=st.number_input(input('轉速資料的收集:限轉12000 =>'))
 confirm_input=st.button('輸入確認')
-    if gas <= car_simulation.get('gas_warning'):
+
+if gas <= car_simulation.get('gas_warning'):
    st.write('油箱只剩', gas, '格! 準備加油!!')
     else:
    st.write('油箱還剩', gas, '格。')
-if speed>=car_simulation.get('speed_limit'):
+    if speed>=car_simulation.get('speed_limit'):
      st.write('即將超速')
-else:
+    else:
      st.write('安全')
-if temp>=car_simulation.get('temp_warning'):
+    if temp>=car_simulation.get('temp_warning'):
      st.write('過熱')
-else:
+    else:
      st.write('正常')
-if 轉速>=car_simulation.get('轉速'):
+    if 轉速>=car_simulation.get('轉速'):
      st.write('即將超過轉速')
-else:
+    else:
      st.write('正常')
 
 #輸出的參考：
